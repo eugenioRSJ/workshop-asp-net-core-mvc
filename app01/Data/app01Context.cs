8c4bd1f47e147b8d9d977a594596ad4df5dbe8e9
Eugênio Rocha <eugeniojunio99@gmail.com>
2019-02-21 15:03:54 -0300

    Creat CRUD Scaffolding

---
+++ app01/Data/app01Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace app01.Models
+{
+    public class app01Context : DbContext
+    {
+        public app01Context (DbContextOptions<app01Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<app01.Models.Department> Department { get; set; }
+    }
+}
